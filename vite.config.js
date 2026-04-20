da65123161e7845974f35c67f3cce916603e9e60
Haritha Prageeth <134475404+HarithaPrageeth@users.noreply.github.com>
2026-04-20 18:32:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Important for Vercel
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+  }
 })
